cc43aa5047ed8929e4f42ac014d199108cf6526c
Abdul Rafi <63101627+TakeMeGH@users.noreply.github.com>
2026-02-07 09:52:11 +0700

    Implement level down card ui and progress bar

---
+++ Assets/GGJ/Code/Utils/DummyInputInjector.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using GGJ.Code.Ability;
+using GGJ.Code.UI;
+using UnityEngine;
+
+namespace GGJ.Code.Utils
+{
+    public class DummyInputInjector : MonoBehaviour
+    {
+        [SerializeField]
+        List<SharedAbilityData> sharedAbilityData;
+
+        float _currentProgress = 0;
+        float _maxProgress = 100;
+
+        void Update()
+        {
+            CheckInjectDowngrade();
+            UpdateProgressBar();
+        }
+
+        void CheckInjectDowngrade()
+        {
+            if (UnityEngine.Input.GetKeyDown(KeyCode.I))
+            {
+                LevelDownSelectorUI.Instance.Show(sharedAbilityData.ToArray());
+            }
+        }
+
+        void UpdateProgressBar()
+        {
+            if (UnityEngine.Input.GetKeyDown(KeyCode.J))
+            {
+                _currentProgress += 10;
+                ProgressBarUI.Instance.UpdateProgress(_currentProgress, _maxProgress);
+                if (_currentProgress >= _maxProgress)
+                {
+                    LevelDownSelectorUI.Instance.Show(sharedAbilityData.ToArray());
+                    _currentProgress -= _maxProgress;
+                }
+            }
+        }
+    }
+}
